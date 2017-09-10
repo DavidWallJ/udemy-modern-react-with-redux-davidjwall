4d58b6cddf28ed0a5d4ae2576b137a3041615739
DavidWallJ <djkindread@gmail.com>
2017-09-10 15:25:25 +0800

    updated config

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = '?key=5220dec8-15d0-4618-904e-0a56466afde9';
+const API_KEY = '?key=davidjwall1234';
 const API_BASE_URL = 'http://reduxblog.herokuapp.com/api/posts';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
@@ -8,38 +8,38 @@ export const CREATE_POST = 'CREATE_POST';
 export const FETCH_POST = 'FETCH_POST';
 export const DELETE_POST = 'DELETE_POST';
 
-export function fetchPosts(){
-    const apiUrl = `${API_BASE_URL}${API_KEY}`;
-    const request = axios.get(apiUrl);
-    return {
-        type: FETCH_POSTS,
-        payload: request
-    };
+export function fetchPosts() {
+	const apiUrl = `${API_BASE_URL}${API_KEY}`;
+	const request = axios.get(apiUrl);
+	return {
+		type: FETCH_POSTS,
+		payload: request
+	};
 }
 
-export function createPost(newPost){
-    const apiUrl = `${API_BASE_URL}${API_KEY}`;
-    const request = axios.post(apiUrl, newPost);
-    return {
-        type: CREATE_POST,
-        payload: request
-    }
+export function createPost(newPost) {
+	const apiUrl = `${API_BASE_URL}${API_KEY}`;
+	const request = axios.post(apiUrl, newPost);
+	return {
+		type: CREATE_POST,
+		payload: request
+	};
 }
 
-export function fetchPost(id){
-    const apiUrl = `${API_BASE_URL}/${id}${API_KEY}`;
-    const request = axios.get(apiUrl);
-    return {
-        type: FETCH_POST,
-        payload: request
-    };
+export function fetchPost(id) {
+	const apiUrl = `${API_BASE_URL}/${id}${API_KEY}`;
+	const request = axios.get(apiUrl);
+	return {
+		type: FETCH_POST,
+		payload: request
+	};
 }
 
-export function deletePost(id){
-    const apiUrl = `${API_BASE_URL}/${id}${API_KEY}`;
-    const request = axios.delete(apiUrl);
-    return {
-        type: DELETE_POST,
-        payload: request
-    };    
+export function deletePost(id) {
+	const apiUrl = `${API_BASE_URL}/${id}${API_KEY}`;
+	const request = axios.delete(apiUrl);
+	return {
+		type: DELETE_POST,
+		payload: request
+	};
 }
